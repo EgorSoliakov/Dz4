e4346d67b11bc1c4a953d86cfcac5f8e207fe8e7
Egor.S <Egorsoliakov@gmail.com>
2023-05-23 00:00:15 +0400

    All1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,6 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿// Задача 25: Напишите цикл, который принимает 
+//на вход два числа (A и B) и возводит число
+// A в натуральную степень B.
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
@@ -9,3 +11,14 @@ Console.Write("Введите число: ");
 int limit = int.Parse(Console.ReadLine());
 return limit;
 }
+
+int Stepen(double a,double b)
+{
+    int step = Convert.ToInt32(Math.Pow(a,b));
+    return step;
+}
+
+int A = Prompt();
+int B = Prompt();
+
+Console.WriteLine($" -> {Stepen(A, B)}");
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,30 @@
-﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+﻿// Задача 27: Напишите программу, которая 
+//принимает на вход число и выдаёт сумму цифр 
+//в числе.
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+
+int Prompt()
+{
+Console.Write("Введите число: ");
+int limit = int.Parse(Console.ReadLine());
+return limit;
+}
+
+int Res(int a)
+{
+    int res = 0;
+    while (a>0)
+    {
+        
+      int b=a%10;
+      res = res+b;
+      a/=10;
+    }
+    return res;
+}
+
+int A = Prompt();
+Console.WriteLine($"->{Res(A)}");
+
