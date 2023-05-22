d424ae37d4be07fc403444027e8970bb1ad95a3e
Egor.S <Egorsoliakov@gmail.com>
2023-05-23 00:08:48 +0400

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,3 +2,15 @@
 // массив из 8 элементов и выводит их на экран.
 //1, 2, 5, 7, 19, 4, 76, 3 -> [1, 2, 5, 7, 19,4,76,3]
 //6, 1, 33, 43, 12, 3 ,2, 9 -> [6, 1, 33,43,12,3,2,9]
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+        Console.Write($" {res[i]} ");
+    }
+    return res;
+}
+GetArray(8, 10, 25);
